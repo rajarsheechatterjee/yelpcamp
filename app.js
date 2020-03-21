8834307e6b9de582e8f8d544b27af47e8c5314e3
Rajarshee Chatterjee <cool.rj2008@gmail.com>
2020-03-21 15:53:19 +0530

    add port

--- app.js
+++ app.js
@@ -51,7 +51,6 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 
-app.listen(3000, function(){
-// app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(process.env.PORT, process.env.IP, function(){
 console.log("The YelpCamp Server Has Started!");
 });

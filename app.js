db7686267d28bed417ce2c3ff0709d30fe06125b
Rajarshee Chatterjee <cool.rj2008@gmail.com>
2020-03-21 12:06:41 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,57 @@
+var express        = require("express"),
+    app            = express(),
+    bodyParser     = require("body-parser"),
+    mongoose       = require("mongoose"),
+    flash          = require("connect-flash"),
+    passport       = require("passport"),
+    LocalStrategy  = require("passport-local"),
+    methodOverride = require("method-override"),
+    Campground     = require("./models/campground"),
+    Comment        = require("./models/comment"),
+    User           = require("./models/user"),
+    seedDB         = require("./seeds");
+
+var commenntRoutes    = require("./routes/comments"),
+    campgroundsRoutes = require("./routes/campgrounds"),
+    authRoutes        = require("./routes/index");
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
+
+mongoose.connect("mongodb://localhost/yelp_camp");
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(flash());
+app.use(methodOverride("_method"));
+// seedDB(); //seed the database
+
+
+//Passport Configuration
+app.use(require("express-session")({
+    secret: "Once again Rusty wins cutest dog!",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    res.locals.error = req.flash("error");
+    res.locals.success = req.flash("success");
+    next();
+});
+
+//Requiring Routes
+app.use("/", authRoutes);
+app.use("/campgrounds/:id/comments", commenntRoutes);
+app.use("/campgrounds", campgroundsRoutes);
+
+app.listen(3000, function(){
+    console.log("The YelpCamp Server Has Started");
+});

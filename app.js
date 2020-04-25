0343ab345af02be43e5b4d0f45cbb05b9595f711
Rajarshee Chatterjee <cool.rj2008@gmail.com>
2020-04-25 10:14:20 +0530

    First commit

--- app.js
+++ app.js
@@ -51,6 +51,6 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(3000, function(){
 console.log("The YelpCamp Server Has Started!");
 });
